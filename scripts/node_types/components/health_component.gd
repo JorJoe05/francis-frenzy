f60c66d834ec8c901d922d71fcc20fbac3a6145d
JorJoe05 <71857180+JorJoe05@users.noreply.github.com>
2025-01-02 21:39:01 -0500

    ME FUCKING HEART STOPPED

---
+++ scripts/node_types/components/health_component.gd
@@ -0,0 +1,48 @@
+@tool
+class_name HealthComponent
+extends Node
+
+signal damaged(amount)
+signal healed(amount)
+signal depleted
+signal maxed_out
+
+@export var health : int = 5
+@export var minimum_health : int = 0
+@export var maximum_health : int = 5
+
+func heal(amount) -> void:
+	if health == maximum_health:
+		return
+	var amount_healed : int = 0
+	if health < maximum_health:
+		while amount > 0 and health < maximum_health:
+			amount -= 1
+			amount_healed += 1
+			health += 1
+	healed.emit(amount_healed)
+	if health == maximum_health:
+		maxed_out.emit()
+	print("Healed")
+
+func damage(amount) ->  void:
+	if health == minimum_health:
+		depleted.emit()
+		return
+	var amount_damaged : int = 0
+	if health > minimum_health:
+		while amount > 0 and health > minimum_health:
+			amount -= 1
+			amount_damaged += 1
+			health -= 1
+	damaged.emit(amount_damaged)
+	print("Damaged")
+
+func max_out() -> void:
+	var amount = maximum_health - health
+	heal(amount)
+	print("Maxed Out")
+
+func deplete() -> void:
+	var amount = health - minimum_health
+	damage(amount)
