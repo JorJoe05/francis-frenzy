5405e6615e4fd0466f9d41fb8808537d6da7f64e
JorJoe05 <71857180+JorJoe05@users.noreply.github.com>
2025-01-07 02:08:11 -0500

    scene manager

---
+++ bundles/scene_manager/scripts/autoloads/scene_manager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+#region Scene Change
+
+var _startpos : StringName
+
+func set_startpos(startpos : StringName) -> void:
+	_startpos = startpos
+
+func get_startpos() -> StringName:
+	return _startpos
+
+func fade_scene_to_file(path : String, startpos : StringName = &"Default") -> void:
+	var scene = load(path)
+	get_tree().root.add_child(SceneTransition.new(scene, startpos))
+
+func quit_to_title() -> void:
+	fade_scene_to_file("res://Scenes/Menus/Dev Menu/dev_menu.tscn")
+
+#endregion
