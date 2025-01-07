c730b9e309b00a52cdb4f134a2a156facdce0d42
JorJoe05 <71857180+JorJoe05@users.noreply.github.com>
2025-01-07 02:02:54 -0500

    Update hitbox_type_filter.gd

--- scripts/node_types/colliders/hitboxes/interactions/hitbox_type_filter.gd
+++ scripts/node_types/colliders/hitboxes/interactions/hitbox_type_filter.gd
@@ -1,7 +1,7 @@
 class_name HitboxTypeFilter2D
 extends Node
 
-@export var hitbox_type: StringName
+@export var type: StringName
 
 var _parent
 
@@ -20,26 +20,26 @@ func _ready() -> void:
 	_parent.hitbox_shape_exited.connect(_on_parent_hitbox_shape_exited)
 
 func _on_parent_hitbox_entered(hitbox: Hitbox2D) -> void:
-	if hitbox.type == hitbox_type:
+	if hitbox.type == type:
 		hitbox_entered.emit(hitbox)
 
 func _on_parent_hitbox_exited(hitbox: Hitbox2D) -> void:
-	if hitbox.type == hitbox_type:
+	if hitbox.type == type:
 		hitbox_exited.emit(hitbox)
 
 func _on_parent_hitbox_shape_entered(hitbox_rid: RID, hitbox: Hitbox2D, hitbox_shape_index: int, local_shape_index: int) -> void:
-	if hitbox.type == hitbox_type:
+	if hitbox.type == type:
 		hitbox_shape_entered.emit(hitbox_rid, hitbox, hitbox_shape_index, local_shape_index)
 
 func _on_parent_hitbox_shape_exited(hitbox_rid: RID, hitbox: Hitbox2D, hitbox_shape_index: int, local_shape_index: int) -> void:
-	if hitbox.type == hitbox_type:
+	if hitbox.type == type:
 		hitbox_shape_exited.emit(hitbox_rid, hitbox, hitbox_shape_index, local_shape_index)
 
 func get_overlapping_hitboxes() -> Array:
 	var overlapping_hitboxes = _parent.get_overlapping_hitboxes()
 	var out = []
 	for hitbox in overlapping_hitboxes:
-		if hitbox.type == hitbox_type:
+		if hitbox.type == type:
 			out.append(hitbox)
 	return out
 
@@ -48,6 +48,6 @@ func has_overlapping_hitboxes() -> bool:
 	return overlapping_hitboxes.size() > 0
 
 func overlaps_hitbox(hitbox: Hitbox2D) -> bool:
-	if hitbox.type == hitbox_type:
+	if hitbox.type == type:
 		return true
 	return false
